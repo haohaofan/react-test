a4ab8b84964c78b7e4fe862e14b2cda80a070d4d
your <gyufan@163.com>
2018-07-21 21:49:42 +0800

    pc端页头初始化

--- src/js/root.js
+++ src/js/root.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from 'antd';
+import {Button} from 'antd';
+import PCIndex from './components/pc_index';
 import 'antd/dist/antd.css';
 
-import {Route,BrowserRouter, Switch,HashRouter} from 'react-router-dom';
+import {Route, BrowserRouter, Switch, HashRouter} from 'react-router-dom';
+
 // import { Router, Route, Switch } from 'react-router'
 
-class Root extends React.Component{
-  render(){
-      return (
-          <div>
-              <Button type="primary">Primary</Button>
-              <Button>Default</Button>
-              <Button type="dashed">Dashed</Button>
-              <Button type="danger">Danger</Button>
-          </div>
-      );
-  };
+class Root extends React.Component {
+    render() {
+        return (
+            <PCIndex></PCIndex>
+        );
+    };
 }
 
 ReactDOM.render(<Root/>, document.getElementById('mainContainer'));
