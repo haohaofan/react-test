475877223026353946dc42b58b752877c5865f35
Administrator <gyufan@163.com>
2018-08-21 23:03:00 +0800

    头像上传与处理

--- src/js/components/mobile_usercenter.js
+++ src/js/components/mobile_usercenter.js
@@ -21,7 +21,37 @@ import {Link, NavLink} from 'react-router-dom';
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
 export default class MobileUserCenter extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            previewImage: '',
+            previewVisible: false
+        };
+    };
+
+    handleCancel(){
+        this.setState({previewVisible: false})
+    };
 	render() {
+        const props = {
+            action: 'http://newsapi.gugujiankong.com/handler.ashx',
+            headers: {
+                "Access-Control-Allow-Origin":"*"
+            },
+            listType: 'picture-card',
+            defaultFileList:[
+                {
+                    uid:-1,
+                    name:'xxx.png',
+                    state: 'done',
+                    url:'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png',
+                    thumbUrl:'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png'
+                }
+            ],
+            onPreview: (file)=>{
+                this.setState({previewImage:file.url,previewVisible:true});
+            }
+        };
 		return (
 			<div>
 				<MobileHeader/>
@@ -30,7 +60,18 @@ export default class MobileUserCenter extends React.Component {
 						<Tabs>
 							<TabPane tab="我的收藏列表" key="1"></TabPane>
 							<TabPane tab="我的评论列表" key="2"></TabPane>
-							<TabPane tab="头像设置" key="3"></TabPane>
+                            <TabPane tab="头像设置" key="3">
+                                <div className="clearfix">
+                                    <Upload {...props}>
+                                        <Icon type="plus"/>
+                                        <div className="ant-upload-text">上传照片</div>
+                                    </Upload>
+                                    <Modal visible={this.state.previewVisible} footer={null}
+                                           onCancel={this.handleCancel.bind(this)}>
+                                        <img alt="预览" src={this.state.previewImage}/>
+                                    </Modal>
+                                </div>
+							</TabPane>
 						</Tabs>
 					</Col>
 				</Row>
